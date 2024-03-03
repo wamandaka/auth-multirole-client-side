df8f3905eb570ee6f57edc8c714fe7f5118a59e1
wamandaka <108261088+wamandaka@users.noreply.github.com>
2024-03-03 17:09:31 +0700

    bikin fungsi edit user

--- src/components/FormEditUser.tsx
+++ src/components/FormEditUser.tsx
@@ -1,12 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
 
 const FormEditUser = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confPassword, setConfPassword] = useState("");
+  const [role, setRole] = useState("");
+  const [message, setMessage] = useState("");
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  useEffect(() => {
+    const getUserById = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8080/users/${id}`);
+        setName(response.data.name);
+        setEmail(response.data.email);
+        setRole(response.data.role);
+      } catch (error: any) {
+        if (error.response) {
+          setMessage(error.response.data.msg);
+        }
+      }
+    };
+    getUserById();
+  }, [id]);
+
+  const updateUser = async (e: any) => {
+    e.preventDefault();
+    try {
+      await axios.patch(`http://localhost:8080/users/${id}`, {
+        name: name,
+        email: email,
+        password: password,
+        confPassword: confPassword,
+        role: role,
+      });
+      navigate("/users");
+    } catch (error: any) {
+      if (error.response) {
+        setMessage(error.response.data.msg);
+      }
+    }
+  };
   return (
     <div className="">
       <h1 className="text-3xl font-bold text-base-300">Users</h1>
       <h2 className="text-2xl font-bold text-base-300">Edit User</h2>
 
-      <form className="mb-0 mt-6 space-y-4 rounded-lg p-4 shadow-lg sm:p-6 lg:p-8 max-w-screen-sm">
+      <form
+        onSubmit={updateUser}
+        className="mb-0 mt-6 space-y-4 rounded-lg p-4 shadow-lg sm:p-6 lg:p-8 max-w-screen-sm"
+      >
+        <p className="text-red-500">{message}</p>
         <label className="input input-bordered flex items-center gap-2">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -19,6 +67,8 @@ const FormEditUser = () => {
           <input
             name="name"
             type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
             className="grow"
             placeholder="Username"
           />
@@ -36,6 +86,8 @@ const FormEditUser = () => {
           <input
             name="email"
             type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             className="grow"
             placeholder="Email"
           />
@@ -47,17 +99,15 @@ const FormEditUser = () => {
             fill="currentColor"
             className="w-4 h-4 opacity-70"
           >
-            <path
-              fillRule="evenodd"
-              d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-              clipRule="evenodd"
-            />
+            <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+            <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
           </svg>
           <input
-            name="password"
+            name="email"
             type="password"
+            onChange={(e) => setPassword(e.target.value)}
             className="grow"
-            placeholder="Password"
+            placeholder="Password..."
           />
         </label>
         <label className="input input-bordered flex items-center gap-2">
@@ -67,20 +117,22 @@ const FormEditUser = () => {
             fill="currentColor"
             className="w-4 h-4 opacity-70"
           >
-            <path
-              fillRule="evenodd"
-              d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-              clipRule="evenodd"
-            />
+            <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+            <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
           </svg>
           <input
-            name="password"
+            name="email"
             type="password"
+            onChange={(e) => setConfPassword(e.target.value)}
             className="grow"
-            placeholder="Confirm Password"
+            placeholder="Confirm Password..."
           />
         </label>
-        <select className="select select-bordered w-full">
+        <select
+          value={role}
+          onChange={(e) => setRole(e.target.value)}
+          className="select select-bordered w-full"
+        >
           <option disabled selected>
             What is your role?
           </option>
@@ -88,7 +140,9 @@ const FormEditUser = () => {
           <option value="user">User</option>
         </select>
         <div className="w-full text-center">
-          <button className="btn btn-wide">Update</button>
+          <button type="submit" className="btn btn-wide">
+            Update
+          </button>
         </div>
       </form>
     </div>
