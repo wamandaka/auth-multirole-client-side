bc0c6372d261f8ffc07d8a494233a5a353eee2ec
wamandaka <108261088+wamandaka@users.noreply.github.com>
2024-03-03 17:09:44 +0700

    get user

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,16 +1,32 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const UserList = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    const response = await axios.get("http://localhost:8080/users");
+    setUsers(response.data);
+  }
+
+  const deleteUser = async (userId: any) => {
+    await axios.delete(`http://localhost:8080/users/${userId}`);
+    getUsers();
+  }
   return (
     <div>
       <h1 className="text-3xl font-bold text-base-300">User List</h1>
-      <NavLink
+      <Link
         to={"/users/add"}
         className="inline-block rounded bg-indigo-600 px-4 py-2 text-xs font-medium text-white hover:bg-indigo-700 my-4"
       >
         Add new User
-      </NavLink>
+      </Link>
       <div className="overflow-x-auto max-w-screen-lg">
         <table className="table bg-slate-800 rounded-md">
           {/* head */}
@@ -24,27 +40,23 @@ const UserList = () => {
             </tr>
           </thead>
           <tbody>
-            {/* row 1 */}
-            <tr>
-              <th>1</th>
-              <td>Cy Ganderton</td>
-              <td>Quality Control Specialist</td>
-              <td>Blue</td>
-            </tr>
-            {/* row 2 */}
-            <tr>
-              <th>2</th>
-              <td>Hart Hagerty</td>
-              <td>Desktop Support Technician</td>
-              <td>Purple</td>
-            </tr>
-            {/* row 3 */}
-            <tr>
-              <th>3</th>
-              <td>Brice Swyre</td>
-              <td>Tax Accountant</td>
-              <td>Red</td>
-            </tr>
+            {users?.map((user: any, index) => (
+              <tr key={user.uuid}>
+                <th>{index + 1}</th>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.role}</td>
+                <td>
+                  <Link
+                    to={`/users/edit/${user.uuid}`}
+                    className="btn btn-info"
+                  >
+                    Edit
+                  </Link>
+                  <button onClick={() => deleteUser(user.uuid)} className="btn btn-error">Delete</button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
